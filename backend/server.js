2c5d96a9a2ef5c9eb7389697607081c150c827b1
JacobDChamberlain <straightlegmusic@gmail.com>
2024-12-19 15:53:50 -0600

    create stripe checkout session & send items

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,77 @@ const cors = require('cors');
 const { sequelize, Item } = require('./models'); // Import Sequelize instance and Item model
 const app = express();
 const port = process.env.PORT || 5001;
+require('dotenv').config({ path: './.env'});
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 app.use(express.json());
 app.use(cors());
+
+
+app.post('/create-checkout-session', async (req, res) => {
+    const itemsToPurchase = req.body; // Expecting array of { id, quantity }
+    const purchasedItems = [];
+
+    try {
+        // Validate item quantities
+        for (let item of itemsToPurchase) {
+            if (item.quantity < 1) {
+                throw new Error("Invalid item quantity");
+            }
+        }
+
+        // Use a transaction to ensure atomicity
+        await sequelize.transaction(async (t) => {
+            for (const { id, quantity } of itemsToPurchase) {
+                const item = await Item.findByPk(id, { transaction: t });
+
+                if (!item || item.stock < quantity) {
+                    throw new Error(`Requested quantity not available for ${item ? item.name : 'Unknown item'}`);
+                }
+
+                // Decrement the item's stock
+                item.stock -= quantity;
+                await item.save({ transaction: t });
+
+                // Add the item to the list of purchased items
+                purchasedItems.push({
+                    id: item.id,
+                    name: item.name,
+                    price: Math.round(item.price * 100),
+                    quantity
+                });
+            }
+        });
+
+        // Create Stripe checkout session
+        const lineItems = purchasedItems.map(({ name, quantity, price }) => ({
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name,
+                },
+                unit_amount: price,
+            },
+            quantity,
+        }));
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'https://www.maldevera.com/successful-purchase',
+            cancel_url: 'https://www.maldevera.com/sad-yeet',
+        });
+
+        // Respond with session URL
+        res.json({ url: session.url });
+    } catch (error) {
+        console.error('Error in /create-checkout-session:', error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+});
+
 
 
 // Endpoint to get inventory

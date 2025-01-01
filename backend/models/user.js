d536bb89861322998d67246800fb6c71624be647
JacobDChamberlain <straightlegmusic@gmail.com>
2024-12-31 18:15:41 -0600

    model, migrations, seeder for Users table

---
+++ backend/models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        tableName: 'users', // Explicitly specify the table name
+        timestamps: true // Enable Sequelize to handle createdAt and updatedAt
+    });
+
+    return User;
+};
---
+++ backend/seeders/20250101000638-seed-users.js
@@ -0,0 +1,25 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      {
+          username: 'J-Man',
+          password: await bcrypt.hash('praiseGod94!', 10),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+      },
+      {
+          username: 'PKFire',
+          password: await bcrypt.hash('tarantula666!', 10),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};

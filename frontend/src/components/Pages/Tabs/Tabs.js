d278b2ff24c7a4a818c5f6466c595bfd048093cb
JacobDChamberlain <straightlegmusic@gmail.com>
2025-03-23 01:34:29 -0500

    TABS ARE VIEWABLE HOLY SHIT - needs work to look good on mobile BUT STILL

--- frontend/src/components/Pages/Tabs/Tabs.js
+++ frontend/src/components/Pages/Tabs/Tabs.js
@@ -4,9 +4,17 @@ import './Tabs.css';
 export default function Tabs() {
     return (
         <div className="tabs-wrapper">
-            <h2 className='tabs-page-header'>Tabs:</h2>
+            <h2 className='tabs-page-header'>⚠️ Under Construction ⚠️</h2>
+            <h5 className='tabs-page-header'>Mobile view coming soon!</h5>
+            <h6 className='tabs-page-header'>Also don't steal our shit</h6>
             <div className='tabs-body'>
-                Tabs displayed here...
+                <iframe
+                    src="https://maldetabviewer.onrender.com/"
+                    title="Malde Tab Viewer"
+                    width="100%"
+                    height="800"
+                    style={{ border: 'none' }}
+                />
             </div>
         </div>
     )

27f416bddd140ec3b4f0e265aee0810f4be93055
JacobDChamberlain <straightlegmusic@gmail.com>
2025-08-19 15:25:28 -0500

    updated flyer, location for aug25, added show info sept 4

--- frontend/src/components/Pages/Shows/Shows.js
+++ frontend/src/components/Pages/Shows/Shows.js
@@ -19,7 +19,8 @@ import may17_2025 from './Flyers/may17thRGFlyer.jpg'
 import may24_2025 from './Flyers/may24-2025-DW.png';
 import july23_2025 from './Flyers/july23_2025.jpeg';
 import aug8_2025 from './Flyers/aug8thzounds.jpeg';
-import aug25_2025 from './Flyers/aug25th2025.jpg'
+import aug25_2025 from './Flyers/aug25_texasTeaRoom.jpeg';
+import sept4th_2025 from './Flyers/sept4_haltomTheater.jpeg';
 
 export default function Shows() {
 
@@ -41,8 +42,9 @@ export default function Shows() {
         { flyer: '.' + may17_2025, alt: 'may_17th_2025_RubberGloves', date: 'May 17th, 2025', venue: 'Rubber Gloves', address: '411 E Sycamore St, Denton, TX 76205', bands: ['Pale Misery', 'Tsuris', 'Nocturnal Spawn'] },
         { flyer: '.' + may24_2025, alt: 'may_24th_2025_DoubleWide', date: 'May 24th, 2025', venue: 'DoubleWide', address: '3510 Commerce St, Dallas, TX', bands: ['Volcandra', 'Saidan', 'Inverted Candles'] },
         { flyer: '.' + july23_2025, alt: 'july_23_2025_RubberGloves', date: 'July 23rd, 2025', venue: 'Rubber Gloves', address: '411 E Sycamore St, Denton, TX 76205', bands: ['Eternal', 'Nocturnal Spawn', 'Repulse'] },
-        { flyer: '.' + aug8_2025, alt: 'aug_8th_2025_ZoundsBSide', date: 'Aug 8th, 2025', venue: 'Zounds B-Side', address: '10050 Shoreview Rd, Dallas, TX 75238', bands: ['Throat Locust', 'Animus'] },
-        { flyer: '.' + aug25_2025, alt: 'aug_25th_TBD', date: 'August 25th, 2025', venue: 'TBD', address: '', bands: ['Panpsychism', 'Psychiatric Regurgitation', 'Cynical'] }
+        { flyer: '.' + aug8_2025, alt: 'aug_8th_2025_ZoundsBSide', date: 'Aug 8th, 2025', venue: 'Zounds B-Side', address: '10050 Shoreview Rd, Dallas, TX 75238', bands: ['Throat Locust', 'Animus', 'No Gimmicks'] },
+        { flyer: '.' + aug25_2025, alt: 'aug_25th_TXTeaRoom', date: 'August 25th, 2025', venue: 'TX Tea Room', address: '2815 Main St Suite B, Dallas, TX 75226', bands: ['Panpsychism', 'Psychiatric Regurgitation', 'Cynical'] },
+        { flyer: '.' + sept4th_2025, alt: 'sept_4th_HaltomTheater', date: 'September 4th, 2025', venue: 'Haltom Theater', address: '5601 E Belknap St, Haltom City, TX 76117', bands: ['Labyrinth', 'Warhog', 'Skull Archer', 'Odious'] }
     ];
 
     return (

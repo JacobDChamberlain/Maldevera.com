b6db7a8203df28d725d8d8300fce0d25e7fc7e85
JacobDChamberlain <straightlegmusic@gmail.com>
2026-05-14 21:03:02 -0500

    add fancy border, center flyer within border, click to expand (like shows)

--- frontend/src/components/Pages/Tours/Tours.js
+++ frontend/src/components/Pages/Tours/Tours.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from 'react-modal';
 import './Tours.css'
 import tourFlyer from './TourFlyers/Maldevera-East-Coast-Tour-2026-Flyer.png'
+import { FancyBorder } from '../../Templates/FancyBorder'
 
 export default function Tours() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className="tours-wrapper">
-            <img
-                src={tourFlyer}
-                alt="Maldevera East Coast Tour 2026"
-                className="tour-flyer"
-            />
+            <FancyBorder>
+                <img
+                    src={tourFlyer}
+                    alt="Maldevera East Coast Tour 2026"
+                    className="tour-flyer"
+                    onClick={() => setIsOpen(true)}
+                />
+            </FancyBorder>
+            <ReactModal
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+                className='flyer-modal'
+                overlayClassName='flyer-overlay'
+            >
+                <img
+                    src={tourFlyer}
+                    alt='fullscreen flyer'
+                    className='modal-flyer-image'
+                    onClick={() => setIsOpen(false)}
+                />
+            </ReactModal>
         </div>
     )
 }
